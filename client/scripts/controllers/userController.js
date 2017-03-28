4770444e26ec6cb27120b5b25fb71385b881bf25
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2017-03-28 08:54:23 +0300

    Prelaoder

--- client/scripts/controllers/userController.js
+++ client/scripts/controllers/userController.js
@@ -10,21 +10,25 @@ class UserController {
     renderLoginTemplate(content, context) {
         var $content = content;
 
+        this.utils.showPreloader();
         this.utils.checkAuthenticated()
             .then((result) => {
 
                 if(result.success) {
                     context.redirect('#/dashboard');
+                    this.utils.hidePreloader(0);
                 }
                 else {
                     this.template.getTemplate('login-template')
                         .then((resultTemplate) => {
                             $content.html(resultTemplate);
+                            this.utils.hidePreloader(500);
                         });
                 }
             })
             .catch(() => {
                 context.redirect('#/home');
+                this.utils.hidePreloader(0);
             });
     }
 
@@ -33,6 +37,7 @@ class UserController {
             data = context.params,
             errorsPlaceholder = $('#login-form-errors');
 
+        this.utils.showPreloader();
         this.requester.post('/api/auth/login', data)
             .then((result) => {
 
@@ -42,9 +47,11 @@ class UserController {
 
                     localStorage.setItem('auth_token', result.auth_token);
                     context.redirect('#/dashboard');
+                    this.utils.hidePreloader(500);
                 } else {
                     errorsPlaceholder.html(result.message);
                     errorsPlaceholder.show();
+                    this.utils.hidePreloader(500);
                 }
             });
     }
@@ -52,16 +59,19 @@ class UserController {
     renderRegisterTemplate(content, context) {
         var $content = content;
 
+        this.utils.showPreloader();
         this.utils.checkAuthenticated()
             .then((result) => {
 
                 if(result.success) {
                     context.redirect('#/profile');
+                    this.utils.hidePreloader(500);
                 }
                 else {
                     this.template.getTemplate('register-template')
                         .then((resultTemplate) => {
                             $content.html(resultTemplate);
+                            this.utils.hidePreloader(500);
                         });
                 }
             })
@@ -76,16 +86,19 @@ class UserController {
             errorsPlaceholder = $('#register-form-errors'),
             errorsPlaceholderList = $('#register-form-errors-list');
 
+        this.utils.showPreloader();
         this.requester.post('/api/auth/register', data)
             .then((result) => {
                 if (result.success) {
                     errorsPlaceholderList.html('');
                     context.redirect('#/login');
+                    this.utils.hidePreloader(500);
                 } else {
                     this.utils.displayErrorsList(
                         errorsPlaceholder,
                         errorsPlaceholderList,
                         result.validationErrors);
+                    this.utils.hidePreloader(500);
                 }
             });
     }
